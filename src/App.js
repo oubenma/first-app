133d4cd8104dae942ea0969774dc8abbaabf5b86
mohamed oubenma <moubenma@gmail.com>
2020-05-25 22:49:54 +0000

    first app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,52 @@
 import React from 'react';
-import logo from './logo.svg';
+import { useState } from 'react';
+import Books from './Books';
 import './App.css';
 
 function App() {
+  const [numberOfBooks, setnumberOfBooks] = useState(0);
+  const [currentBook, setCurrentBook] = useState('');
+  const [listBooks, setListBooks] = useState([]);
+
+  const incrementBooks = () => {
+    setnumberOfBooks(numberOfBooks + 1);
+  };
+
+  const deleteLastBook = () => {
+    setListBooks(listBooks.slice(0, listBooks.length - 1));
+  };
+
+  const addBook = () => {
+    // 39lo 3lih mzyan: normalement khassna nssifto tableau machi string
+    setListBooks([...listBooks, currentBook]);
+  };
+
+  const textChanged = (e) => {
+    setCurrentBook(e.target.value);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className='maindiv'>
+        <span>number of books: {numberOfBooks} </span>
+        <br />
+        <button onClick={incrementBooks}>Incrementer</button>
+
+        <button onClick={deleteLastBook}>Supprimer</button>
+        <br />
+        <br />
+        <input
+          type='textfield'
+          placeholder='rich dad poor dad'
+          onChange={textChanged}
+        />
+        <button onClick={addBook}>Ajouter</button>
+
+        {listBooks.map((item) => {
+          return <Books bookname={item} />;
+        })}
+      </div>
+    </>
   );
 }
 
